2040d737eb1f21a9e452647c7d6086b50dbe4780
Felipe <f.a.janotte@gmail.com>
2020-05-19 09:39:12 -0300

    Aula:02

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
- var nome = "Felipe Augusto Janotte";
- var n1 = 4;
- var n2 = 4;
- var frase = "Japão é o melhor time do mundo"
+var frutas = [{nome:"Maçã", cor:"Vermelha"}, {nome:"Uva", cor:"Roxa"}];
+console.log(frutas)
+console.log(frutas[1].nome)
+
+
+var fruta = {nome:"Maçã", cor:"vermelho"};
+console.log(fruta.nome);
+console.log(fruta.cor);
+
+var lista = ["Maçã", "Pera", "Laranja"];
+lista.pop();
+lista.push("Uva");
+console.log(lista[1]);
+console.log(lista);
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+console.log(lista.length);
+console.log(lista.toString());
+console.log(lista.join(" - "))
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "))
+
+
+//var nome = "Felipe Augusto Janotte";
+ //var n1 = 4;
+ //var n2 = 4;
+ //var frase = "Japão é o melhor time do mundo"
  //alert(nome + " tem " + n1 * n2 + " anos")
  //alert(idade + idade2);
- console.log(nome);
- console.log(n1 * n2)
- console.log(frase.toLocaleLowerCase());
+ //console.log(nome);
+ //console.log(n1 * n2)
+ //console.log(frase.toLocaleLowerCase());
  //alert(frase.replace("Japão", "Brasil"))

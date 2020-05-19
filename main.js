0a41b56d8f69e89e9660b6426654cff66db3b802
Felipe <f.a.janotte@gmail.com>
2020-05-19 10:49:36 -0300

    Aula:04

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 /*
+Aula:04
+function soma(n1, n2){
+  return n1 + n2
+}
+alert(soma(5, 10));
+
+function validaIdade(idade){
+  var validar;
+  if (idade >= 18){
+    validar = true
+  }else{
+    validar = false
+  }
+  return validar;
+}
+
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade));
+*/
+
+/*
 Aula:03
 var idade = prompt("Qual a sua idade?");
 if (idade >= 18){

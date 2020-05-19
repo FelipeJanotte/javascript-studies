76317c14c8749d5017564df28208b93b75b8a28b
Felipe <f.a.janotte@gmail.com>
2020-05-19 10:02:59 -0300

    Aula:03

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
+/*
+Aula:03
+var idade = prompt("Qual a sua idade?");
+if (idade >= 18){
+  alert("Maior de idade");
+}else{
+  alert("Menor de idade");
+}
+
+var count = 0;
+while(count <= 5){
+  console.log (count);
+  //count = count + 1
+  count ++;
+}
+
+var count ;
+for(count = 0; cuont <= 5; count++){
+  console.log(count)
+};
+
+var d = new Date();
+console.log(d.getHours())
+console.log(d.getMinutes())
+console.log(d.getSeconds())
+console.log(d.getDay())
+*/
+
+/*
+Aula:02
 var frutas = [{nome:"Maçã", cor:"Vermelha"}, {nome:"Uva", cor:"Roxa"}];
 console.log(frutas)
 console.log(frutas[1].nome)
-
 
 var fruta = {nome:"Maçã", cor:"vermelho"};
 console.log(fruta.nome);
@@ -20,15 +49,18 @@ console.log(lista.join(" - "))
 console.log(lista.reverse());
 console.log(lista.toString());
 console.log(lista.join(" - "))
+*/
 
-
-//var nome = "Felipe Augusto Janotte";
- //var n1 = 4;
- //var n2 = 4;
- //var frase = "Japão é o melhor time do mundo"
- //alert(nome + " tem " + n1 * n2 + " anos")
- //alert(idade + idade2);
- //console.log(nome);
- //console.log(n1 * n2)
- //console.log(frase.toLocaleLowerCase());
- //alert(frase.replace("Japão", "Brasil"))
+/*
+Aula:01
+var nome = "Felipe Augusto Janotte";
+var n1 = 4;
+var n2 = 4;
+var frase = "Japão é o melhor time do mundo"
+alert(nome + " tem " + n1 * n2 + " anos")
+alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2)
+console.log(frase.toLocaleLowerCase());
+alert(frase.replace("Japão", "Brasil"))
+*/

2dc62a26d3b1d60c87fb6ac685c52ecbd0a84f8b
Felipe <f.a.janotte@gmail.com>
2020-05-19 09:24:17 -0300

    Aula:01

---
+++ main.js
@@ -0,0 +1,10 @@
+ var nome = "Felipe Augusto Janotte";
+ var n1 = 4;
+ var n2 = 4;
+ var frase = "Japão é o melhor time do mundo"
+ //alert(nome + " tem " + n1 * n2 + " anos")
+ //alert(idade + idade2);
+ console.log(nome);
+ console.log(n1 * n2)
+ console.log(frase.toLocaleLowerCase());
+ //alert(frase.replace("Japão", "Brasil"))
